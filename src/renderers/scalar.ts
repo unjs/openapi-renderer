01ef879bc4faeebd63aa4d02f97ed3508cdb3f41
Hans Pagel <mail@hanspagel.com>
2025-07-03 12:25:26 +0200

    refactor: directly render the HTML

--- src/renderers/scalar.ts
+++ src/renderers/scalar.ts
@@ -1,7 +1,4 @@
-import {
-  getHtmlDocument,
-  type HtmlRenderingConfiguration as ScalarConfig,
-} from "@scalar/core/libs/html-rendering";
+import type { ApiReferenceConfiguration as ScalarConfig } from "@scalar/api-reference";
 import type { RenderHTMLOptions } from "../types.ts";
 
 // https://github.com/scalar/scalar
@@ -13,10 +10,26 @@ export default function render(opts: RenderHTMLOptions): string {
 
   const scalarConfig: ScalarConfig = {
     ...opts.scalar,
-    pageTitle: opts.meta?.title || "OpenAPI Documentation",
-    cdn: CDN_URL,
     url: opts.spec,
   };
 
-  return /* html */ getHtmlDocument(scalarConfig, opts.styles);
+  return /* html */ `<!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="${opts.meta?.description}" />
+        <title>${opts.meta?.title}</title>
+        <style>${opts.styles}</style>
+      </head>
+      <body>
+        <div id="app"></div>
+
+        <script src="${CDN_URL}"></script>
+
+        <script>
+          Scalar.createApiReference('#app', ${JSON.stringify(scalarConfig)})
+        </script>
+      </body>
+    </html>`;
 }
